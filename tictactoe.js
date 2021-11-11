b9297b5ccebb7f28a1a0fc8050efaf1126e0e64f
HeonwooShin <62944438+HeonwooShin@users.noreply.github.com>
2021-11-11 17:08:08 +0900

    update

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,9 @@
-const one = document.getElementById('1'),
-two = document.getElementById('2'),
-three = document.getElementById('3'),
-four = document.getElementById('4'),
-five = document.getElementById('5'),
-six = document.getElementById('6'),
-seven = document.getElementById('7'),
-eight = document.getElementById('8'),
-nine = document.getElementById('9'),
-
+const cells = document.querySelectorAll('.cell'),
 winMessage = document.querySelector('.win'),
 reset = document.querySelector('.reset');
 
 let turn = 0;
+let someoneWon = false;
 
 const winArray = [
     [1,2,3],
@@ -29,18 +21,14 @@ let Oarray = [];
 
 reset.addEventListener('click', function() {
     turn = 0;
+    someoneWon = false;
     Xarray = [];
     Oarray = [];
-    one.innerHTML="";
-    two.innerHTML="";
-    three.innerHTML="";
-    four.innerHTML="";
-    five.innerHTML="";
-    six.innerHTML="";
-    seven.innerHTML="";
-    eight.innerHTML="";
-    nine.innerHTML="";
-    winMessage.innerHTML="";
+    winMessage.classList.add('hidden')
+    cells.forEach(cell => {
+        cell.innerHTML = "";
+    });
+    winMessage.innerHTML = "";
 })
 
 function checkWin() {
@@ -48,144 +36,34 @@ function checkWin() {
         Xintersection = Xarray.filter(x => winArray[i].includes(x));
         Ointersection = Oarray.filter(x => winArray[i].includes(x));
         if(JSON.stringify(Xintersection.sort()) == JSON.stringify(winArray[i])){
-            winMessage.innerHTML="X is Winner!";
+            winMessage.classList.remove('hidden')
+            winMessage.innerHTML = "X won!";
+            someoneWon = true
         } else if(JSON.stringify(Ointersection.sort()) == JSON.stringify(winArray[i])){
-            winMessage.innerHTML="O is Winner!";
+            winMessage.classList.remove('hidden')
+            winMessage.innerHTML = "O won!";
+            someoneWon = true
         }
     }
+    if(turn == 9 && !someoneWon){
+        winMessage.classList.remove('hidden')
+        winMessage.innerHTML = "Draw!"
+    }
 }
 
-one.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(1) && !Oarray.includes(1) && winMessage.innerHTML == "") {//winMessage가 비어있다면 if문 추가
-    if(turn % 2 == 0) {
-        one.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(1);
-    } else {
-        one.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(1);
-    }
-    
-    turn += 1;
-    checkWin();
-}
-});
-
-two.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(2) && !Oarray.includes(2) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        two.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(2);
-    } else {
-        two.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(2);
-    }
-  
-    turn += 1;
-    checkWin();
-    }
-});
-
-three.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(3) && !Oarray.includes(3) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        three.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(3);
-    } else {
-        three.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(3);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-four.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(4) && !Oarray.includes(4) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        four.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(4);
-    } else {
-        four.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(4);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-five.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(5) && !Oarray.includes(5) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        five.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(5);
-    } else {
-        five.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(5);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-six.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(6) && !Oarray.includes(6) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        six.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(6);
-    } else {
-        six.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(6);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-seven.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(7) && !Oarray.includes(7) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        seven.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(7);
-    } else {
-        seven.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(7);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-eight.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(8) && !Oarray.includes(8) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        eight.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(8);
-    } else {
-        eight.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(8);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
-
-nine.addEventListener('click', function(){
-    if(turn < 9 && !Xarray.includes(9) && !Oarray.includes(9) && winMessage.innerHTML == "") {
-    if(turn % 2 == 0) {
-        nine.innerHTML = "<img src='x.png' width=100px height=100px>";
-        Xarray.push(9);
-    } else {
-        nine.innerHTML = "<img src='o.png' width=100px height=100px>";
-        Oarray.push(9);
-    }
-    
-    turn += 1;
-    checkWin();
-    }
-});
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', ()=>{
+        if(turn < 9 && !Xarray.includes(i+1) && !Oarray.includes(i+1) && winMessage.innerHTML == "") {//winMessage가 비어있다면 if문 추가
+            if(turn % 2 == 0) {
+                cells[i].innerHTML = "<img src='x.png' class='OorX' width=160px height=160px>";
+                Xarray.push(i+1);
+            } else {
+                cells[i].innerHTML = "<img src='o.png' class='OorX' width=160px height=160px>";
+                Oarray.push(i+1);
+            }
+            
+            turn += 1;
+            checkWin();
+        } 
+    });
+};
